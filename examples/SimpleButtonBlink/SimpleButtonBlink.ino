db7e682e59a308bb230b02ed0877b7a5d8f608d1
Jonathan Bartlett <jonathan.l.bartlett@gmail.com>
2016-07-30 06:44:30 -0500

    Fixed bugs

--- examples/SimpleButtonBlink/SimpleButtonBlink.ino
+++ examples/SimpleButtonBlink/SimpleButtonBlink.ino
@@ -21,7 +21,7 @@ bool blink() {
 
 bool set_speed() {
   mgr.resetContext();
-  mgr.addListener(new EvtPinListener(BUTTON_PIN, 50, (EvtAction)set_speed));
+  mgr.addListener(new EvtPinListener(BUTTON_PIN, (EvtAction)set_speed));
   speed = !speed; // Change speeds
   if(speed == HIGH) {
     mgr.addListener(new EvtTimeListener(250, true, (EvtAction)blink));
